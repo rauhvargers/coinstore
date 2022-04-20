f60e702a9f1c23c5ab0125b35e52a27705cb610a
Krišs Rauhvargers <kriss.rauhvargers@lu.lv>
2022-04-20 23:01:11 +0300

    Basics of controllers, models, migrations

---
+++ database/migrations/2022_04_08_090914_create_country_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('country', function (Blueprint $table) {
+            $table->id();
+            $table->text("name");
+            $table->text("englishname");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('country');
+    }
+};
