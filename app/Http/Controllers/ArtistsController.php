c0038cac0c38a9aa7f41131fad72659f63a5a295
Krišs Rauhvargers <kriss.rauhvargers@lu.lv>
2022-05-07 22:31:07 +0300

    Lecture 11 - added Breeze; forms & input

--- app/Http/Controllers/ArtistsController.php
+++ app/Http/Controllers/ArtistsController.php
@@ -3,21 +3,87 @@
 namespace App\Http\Controllers;
 
 use App\Models\Artist;
+use Illuminate\Http\Client\Request as ClientRequest;
 use Illuminate\Http\Request;
 
 class ArtistsController extends Controller
 {
 
+    /**
+     * Returns a full list of artists.
+     * Will it work when there are 15'000 artists in the database? I seriously doubt...
+     */
     public function index()
     {
         $artists = Artist::all()->sortBy('name');
         return view("artists", ['artists' => $artists]);
     }
-    //
-    public function show($slug)
+
+    /**
+     * Returns a single artist, identified by URL substring
+     *
+     * @param  string $slug The URL substring after the main route name /artists/
+     * @return
+     */
+    public function show(string $slug)
     {
         $artist = Artist::where('slug', $slug)->firstOrFail();
         return view("artist_view", ['artist' => $artist]);
     }
 
+    /**
+     * create
+     *
+     * @return
+     */
+    public function create()
+    {
+        return view("artist_add");
+    }
+
+
+    public function store(Request $request)
+    {
+                $rules = array(
+            'name' => 'required|min:2|max:191',
+            'slug' => 'required|alpha_dash|min:3|max:191',
+            'description' => 'string|nullable'
+        );
+
+        $validated = $this->validate($request, $rules);
+
+        $artist = new Artist();
+        $artist->name = $validated["name"];
+        $artist->slug = $validated["slug"];
+        $artist->bio = $validated["description"];
+
+        $artist->save();
+
+        return redirect("/artists");
+
+        //dd($validated);
+        //return "nekā nebūs";
+
+        // $rules = array(
+        //     'name' => 'required|min:2|max:191',
+        //     'slug' => 'required|alpha_dash|min:3|max:191',
+        //     'description' => 'string|nullable');
+
+        //     $validated = $this->validate($request, $rules);
+
+
+
+        //
+
+
+        //     //dd($this);
+        //     //code...
+
+
+
+
+
+        //     dd($results);
+        // return "saglabāju!";
+    }
 }
