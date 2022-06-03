855a8651991dd75a75706593dde31b9b45305cbc
Krišs Rauhvargers <kriss.rauhvargers@lu.lv>
2022-06-03 10:34:02 +0300

    Mailables

---
+++ app/Mail/TestEmail.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class TestEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $address = 'kriss@naivist.net';
+        $subject = 'This is a demo!';
+        $name = 'Kriss Rauhvargers';
+
+        return $this->view('emails.test')
+                    ->from($address, $name)
+                    ->cc($address, $name)
+                    ->bcc($address, $name)
+                    ->replyTo($address, $name)
+                    ->subject($subject)
+                    ->with([ 'test_message' => $this->data['message'] ]);
+    }
+}
